c95c32c89ef21954f1b18d2c5d663b3d958594a3
NobuhleNxumalo <nobuhlenxumalo6@gmail.com>
2023-07-16 12:54:24 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ float f;
 printf("Size of a char:%d byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of an int:%d byte(s)\n", (unsigned long) sizeof(b));
 printf("Size of a long int:%d  byte(s)\n", (unsigned long) sizeof(c));
-printf("Size of a long long:%d byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a long long int :%d byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of float:%d byte(s)\n", (unsigned long) sizeof(f));
 return (0);
 }
